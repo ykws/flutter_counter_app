a2ffbb9998f8ef4e7359ee463afae13331a6fe03
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-20 00:13:07 +0900

    Combine multiple tests in a gruop

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,11 +2,25 @@ import 'package:test/test.dart';
 import 'package:flutter_counter_app/counter.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
 
-    counter.increment();
+    test('Counter value should be incremented', () {
+      final counter = Counter();
 
-    expect(counter.value, 1);
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decrement', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
   });
 }
